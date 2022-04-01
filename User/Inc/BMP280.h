318b09aba328cabc52340a21fefdee5fbfb8d34c
dimanchique <ghettorunner@yandex.ru>
2022-04-01 08:33:04 +0300

    Reformat, Coding style, Function details added

--- User/Inc/BMP280.h
+++ User/Inc/BMP280.h
@@ -1,17 +1,17 @@
 #include "I2C_Process.h"
 #include "ModuleCoreTypes.h"
 
-/* Device Info */
+/** Device Info **/
 #define BMP280_ADDRESS 									0x76
 #define BMP280_ID 										0x58
 
-/* Registers Addresses */
+/** Registers Addresses **/
 #define BMP280_TEMP_REGISTER 							0xFA 	//Temperature 0xFA-0xFC (MSB-LSB-XLSB)
 #define BMP280_PRESSURE_REGISTER						0xF7 	//Temperature 0xF7-0xF9 (MSB-LSB-XLSB)
 #define BMP280_CONFIG_REGISTER 							0xF5	//Configuration Register
-#define BMP280_CTRL_REGISTER							0xF4 	//D0-D1 - Power Control
-#define BMP280_STATUS_REGISTER							0xF3 	//D0 and D3 describes data status
-#define BMP280_RESET_REGISTER							0xE0 	//Put 'BMP_SoftReset' here to soft reset chip
+#define BMP280_CTRL_REGISTER							0xF4 	//Control Register (D0-D1 - Power Control)
+#define BMP280_STATUS_REGISTER							0xF3 	//Status Register (D0 and D3 describes data status)
+#define BMP280_RESET_REGISTER							0xE0 	//Put 'BMP_SoftReset' value here to soft reset chip
 #define BMP280_ID_REGISTER								0xD0 	//Device ID Register
 #define BMP280_REGISTER_DIG_T1 							0x88
 #define BMP280_REGISTER_DIG_T2 							0x8A
@@ -25,40 +25,42 @@
 #define BMP280_REGISTER_DIG_P7 							0x9A
 #define BMP280_REGISTER_DIG_P8 							0x9C
 #define BMP280_REGISTER_DIG_P9 							0x9E
-//------------------------------------------------
-//BMP280_OVERSAMPLING settings
+
+/** Oversampling Settings **/
 #define BMP280_SKIPMEASURING 							0x00	//Dont Measure
-#define BMP280_OVERSAMPLINGx1							0x01	//Ultra-Low Power 				16 bit 	//if Pressure x1	-> Temperature x1 (Recomended)
-#define BMP280_OVERSAMPLINGx2 							0x02	//Low Power 							17 bit 	//if Pressure x2	-> Temperature x1 (Recomended)
-#define BMP280_OVERSAMPLINGx4 							0x03	//Standart Resolution 		18 bit 	//if Pressure x4 	-> Temperature x1 (Recomended)
-#define BMP280_OVERSAMPLINGx8 							0x04	//High Resolution 				19 bit 	//if Pressure x8 	-> Temperature x1 (Recomended)
-#define BMP280_OVERSAMPLINGx16							0x05	//Ultra-High Resolution 	20 bit 	//if Pressure x16 -> Temperature x2 (Recomended)
-//Power settings
-#define BMP280_NORMALMODE								0x03	//Continous measuring with period t(standby)
+#define BMP280_OVERSAMPLINGx1							0x01	//Ultra-Low Power           16 bit 	//if Pressure x1	-> Temperature x1 (Recommended)
+#define BMP280_OVERSAMPLINGx2 							0x02	//Low Power 				17 bit 	//if Pressure x2	-> Temperature x1 (Recommended)
+#define BMP280_OVERSAMPLINGx4 							0x03	//Standart Resolution 		18 bit 	//if Pressure x4 	-> Temperature x1 (Recommended)
+#define BMP280_OVERSAMPLINGx8 							0x04	//High Resolution 			19 bit 	//if Pressure x8 	-> Temperature x1 (Recommended)
+#define BMP280_OVERSAMPLINGx16							0x05	//Ultra-High Resolution 	20 bit 	//if Pressure x16   -> Temperature x2 (Recommended)
+
+/** Power Settings **/
+#define BMP280_NORMALMODE								0x03	//Continuous measuring with period T (Standby Time)
 #define BMP280_FORCEDMODE								0x02	//Make one measure then go to Sleep Mode
 #define BMP280_SLEEPMODE								0x00	//DEFAULT! No measurements, low power consumption
-//Standby Time settings
+
+/** Standby Time Settings **/
 #define BMP280_STANDBYTIME1								0x00	//Standby Time = 0.5ms
 #define BMP280_STANDBYTIME2								0x01	//Standby Time = 62.5ms
-#define BMP280_STANDBYTIME3									0x02	//Standby Time = 125ms
-#define BMP280_STANDBYTIME4									0x03	//Standby Time = 250ms
-#define BMP280_STANDBYTIME5									0x04	//Standby Time = 500ms
-#define BMP280_STANDBYTIME6									0x05	//Standby Time = 1000ms
-#define BMP280_STANDBYTIME7									0x06	//Standby Time = 2000ms
-#define BMP280_STANDBYTIME8									0x07	//Standby Time = 4000ms
-//Filter Coefficient settings
-#define BMP280_NOFILTERING									0x00 //Measure without filtering
-#define BMP280_FILTERCOEFF2									0x01 //IIR Coefficient = 2  (Bandwidth = 0.223*ODR)
-#define BMP280_FILTERCOEFF4									0x02 //IIR Coefficient = 4  (Bandwidth = 0.092*ODR)
-#define BMP280_FILTERCOEFF8									0x03 //IIR Coefficient = 8  (Bandwidth = 0.042*ODR)
-#define BMP280_FILTERCOEFF16								0x04 //IIR Coefficient = 16 (Bandwidth = 0.021*ODR)
-//Status and others
-#define BMP280_SOFTRESET 										0xB6 //Put IC to SoftReset Mode		
-#define BMP280_IS_UPDATING									0x01 //BMP280 updating
+#define BMP280_STANDBYTIME3								0x02	//Standby Time = 125ms
+#define BMP280_STANDBYTIME4								0x03	//Standby Time = 250ms
+#define BMP280_STANDBYTIME5								0x04	//Standby Time = 500ms
+#define BMP280_STANDBYTIME6								0x05	//Standby Time = 1000ms
+#define BMP280_STANDBYTIME7								0x06	//Standby Time = 2000ms
+#define BMP280_STANDBYTIME8								0x07	//Standby Time = 4000ms
 
-#define SEALEVELPRESSURE_PA 								1013250
-//------------------------------------------------
-/* Calibration Data Typedef Struct */
+/** Filter Coefficients Settings **/
+#define BMP280_NOFILTERING								0x00    //Measure without filtering
+#define BMP280_FILTERCOEFF2								0x01    //IIR Coefficient = 2  (Bandwidth = 0.223*ODR)
+#define BMP280_FILTERCOEFF4								0x02    //IIR Coefficient = 4  (Bandwidth = 0.092*ODR)
+#define BMP280_FILTERCOEFF8								0x03    //IIR Coefficient = 8  (Bandwidth = 0.042*ODR)
+#define BMP280_FILTERCOEFF16							0x04    //IIR Coefficient = 16 (Bandwidth = 0.021*ODR)
+
+/** Value to reset BMP 280 and value to determine updating status **/
+#define BMP280_SOFTRESET 								0xB6    //Put IC to SoftReset Mode
+#define BMP280_IS_UPDATING								0x01    //BMP280 updating
+
+/** Calibration Data Typedef Struct **/
 typedef struct{
   uint16_t dig_T1;
   int16_t dig_T2;
@@ -78,23 +80,24 @@ typedef struct{
   int16_t dig_H4;
   int16_t dig_H5;
   int8_t dig_H6;
-} BMP280_CalibData;
-//------------------------------------------------
-/* Typedef Struct */
+} BMP280_CalibrationData;
+
+/** BMP280 Device Struct **/
 typedef struct{
-	I2C_DeviceStruct Communicator;	
-	BMP280_CalibData Coefficients;
-	
-	uint8_t FilterCoefficient;
-	uint8_t Temperature_Oversampling;
-	uint8_t Pressure_Oversampling;
-	uint8_t StandbyTime;
-	uint8_t Power;	
-	
+	I2C_DeviceStruct Communicator;
+    struct
+    {
+        uint8_t FilterCoefficient;
+        uint8_t Temperature_Oversampling;
+        uint8_t Pressure_Oversampling;
+        uint8_t StandbyTime;
+        uint8_t Power;
+    } Configuration;
+    BMP280_CalibrationData Coefficients;
 	struct BarometerData Data;	
 	char DataRepr[50];
 } BMP280_TypeDef;
 
-//---Function Prototypes---//
+/** Functions Prototypes **/
 void BMP_Init(void);
 void BMP_ReadData(void);
--- User/Src/BMP280.c
+++ User/Src/BMP280.c
@@ -1,22 +1,39 @@
 #include "BMP280.h"
 #include "Logger.h"
-#include "string.h"
-//------------------------------------------------
+
 BMP280_TypeDef BMP280 = {0};
 static int32_t temper_int;
-extern volatile DiskWriter Logger;
-//------------------------------------------------
-static uint8_t BMP_Status(){
+extern DiskWriter Logger;
+
+/**
+  * @brief Reading BMP280 status to check if it's updating data
+  * @param None
+  * @retval uint8_t 1 if it's updating (see BMP280.h) and 0 if if doesn't
+  */
+static uint8_t BMP_IsUpdating(void)
+{
 	uint8_t status;
 	I2C_ReadData8(&BMP280.Communicator, BMP280_STATUS_REGISTER, &status);
-	return status&0x09;
+	return (status&0x09)&BMP280_IS_UPDATING;
 }
-//------------------------------------------------
-static void BMP_SoftReset(){
+
+/**
+  * @brief Soft reset of BMP280
+  * @param None
+  * @retval None
+  */
+static void BMP_SoftReset(void){
     I2C_WriteData8(&BMP280.Communicator, BMP280_RESET_REGISTER, BMP280_SOFTRESET);
 }
-//------------------------------------------------
-static void GenerateDataRepresentation(uint8_t ConnectionValid){
+
+/**
+  * @brief Generating string data representation for BMP280 module
+  * @note This function will fill data with zeros if bad connection detected
+  * @param ConnectionValid – uint8_t (BMP280.Communicator->ConnectionStatus == HAL_OK)
+  * @retval None
+  */
+static void GenerateDataRepresentation(uint8_t ConnectionValid)
+{
 	if(ConnectionValid)
 		sprintf(BMP280.DataRepr,
 						"[%s] %.2f; %.3f; %.3f; %.3f;",
@@ -28,16 +45,29 @@ static void GenerateDataRepresentation(uint8_t ConnectionValid){
 	else
 		sprintf(BMP280.DataRepr, "[%s] NULL;", BMP280.Communicator.Name);
 }
-//------------------------------------------------
+
+/**
+  * @brief Write base value of pressure (mmHg) on initialize process (ground) to compute pressure in future
+  * @note This function calls BMP_ReadData first
+  * @param None
+  * @retval None
+  */
 static void BMP_Calibrate(void){
 	BMP_ReadData();
 	BMP280.Data.base_mmHg = BMP280.Data.mmHg;
 }
-//------------------------------------------------
-static void BMP_ReadCoefficients(){
+
+/**
+  * @brief Reading coefficients registers to fill coefficients table (need for computing data)
+  * @param None
+  * @retval None
+  */
+static void BMP_ReadCoefficients(void)
+{
 	if (BMP280.Communicator.ConnectionStatus == HAL_OK)
 		CheckDeviceState(&BMP280.Communicator);
-	if (BMP280.Communicator.ConnectionStatus == HAL_OK){
+	if (BMP280.Communicator.ConnectionStatus == HAL_OK)
+    {
 		I2C_ReadDataU16(&BMP280.Communicator, BMP280_REGISTER_DIG_T1,&BMP280.Coefficients.dig_T1);
 		I2C_ReadDataS16(&BMP280.Communicator, BMP280_REGISTER_DIG_T2,&BMP280.Coefficients.dig_T2);
 		I2C_ReadDataS16(&BMP280.Communicator, BMP280_REGISTER_DIG_T3,&BMP280.Coefficients.dig_T3);
@@ -52,34 +82,47 @@ static void BMP_ReadCoefficients(){
 		I2C_ReadDataS16(&BMP280.Communicator, BMP280_REGISTER_DIG_P9,&BMP280.Coefficients.dig_P9);
 	}
 }
-//------------------------------------------------
-void BMP_Init(void){
-	//Communication Section
+
+/**
+  * @brief Initialization function
+  * @note
+  * This function is writing setup data to Device Struct. \n
+  * If connection is OK data is writing to BMP280 registers. \n
+  * Logging setup result to SD Card
+  * @param None
+  * @retval None
+  */
+void BMP_Init(void)
+{
+	/** Communication Section **/
 	BMP280.Communicator.Name = "BMP280";
 	BMP280.Communicator.State = NotInitialized;
 	BMP280.Communicator.CommAddress = CommunicationAddress(BMP280_ADDRESS);
 	BMP280.Communicator.FactAddress = BMP280_ADDRESS;
 	BMP280.Communicator.Device_ID = BMP280_ID;
-	BMP280.Communicator.ID_Register = BMP280_ID_REGISTER;		
-	//Setup Section
-	BMP280.Power = BMP280_NORMALMODE;														//Measure Continuously
-	BMP280.StandbyTime = BMP280_STANDBYTIME2;										//Measure every 125ms
-	BMP280.Pressure_Oversampling = BMP280_OVERSAMPLINGx4;				//Use Standart Resolution (18 bit)
-	BMP280.Temperature_Oversampling = BMP280_OVERSAMPLINGx1;		//Use Ultra-Low Power mode (16 bit)
-	BMP280.FilterCoefficient = BMP280_FILTERCOEFF8;							//ODR = 7.3 Hz, RMS = 6.4 (see page 14, table 7 of datasheet)	
-	LogState(BMP280.Communicator);	
-	
+	BMP280.Communicator.ID_Register = BMP280_ID_REGISTER;
+
+    /** Communication Section **/
+	BMP280.Configuration.Power = BMP280_NORMALMODE;								//Measure Continuously
+	BMP280.Configuration.StandbyTime = BMP280_STANDBYTIME2;						//Measure every 62.5ms
+	BMP280.Configuration.Pressure_Oversampling = BMP280_OVERSAMPLINGx4;			//Use Standart Resolution (18 bit)
+	BMP280.Configuration.Temperature_Oversampling = BMP280_OVERSAMPLINGx1;		//Use Ultra-Low Power mode (16 bit)
+	BMP280.Configuration.FilterCoefficient = BMP280_FILTERCOEFF8;				//ODR = 7.3 Hz, RMS = 6.4 (see page 14, table 7 of datasheet)
+	LogState(BMP280.Communicator);
+
+    /** Setup Section **/
 	CheckDeviceState(&BMP280.Communicator);
 	if (BMP280.Communicator.ConnectionStatus == HAL_OK)
 	{
 		Verify_Device(&BMP280.Communicator);
-		if (BMP280.Communicator.ConnectionStatus == HAL_OK){
+		if (BMP280.Communicator.ConnectionStatus == HAL_OK)
+        {
 			BMP_SoftReset();
-			while(BMP_Status() & BMP280_IS_UPDATING);
+			while(BMP_IsUpdating()) {};
 			BMP_ReadCoefficients();
-			uint8_t ConfigRegister = (uint8_t)(BMP280.StandbyTime<<5 | BMP280.FilterCoefficient<<2);
+			uint8_t ConfigRegister = (uint8_t)(BMP280.Configuration.StandbyTime<<5 | BMP280.Configuration.FilterCoefficient<<2);
             I2C_WriteData8(&BMP280.Communicator, BMP280_CONFIG_REGISTER, ConfigRegister);
-			uint8_t CtrlRegister = (uint8_t)(BMP280.Temperature_Oversampling<<5 | BMP280.Pressure_Oversampling<<2 | BMP280.Power);
+			uint8_t CtrlRegister = (uint8_t)(BMP280.Configuration.Temperature_Oversampling<<5 | BMP280.Configuration.Pressure_Oversampling<<2 | BMP280.Configuration.Power);
             I2C_WriteData8(&BMP280.Communicator, BMP280_CTRL_REGISTER, CtrlRegister);
 			HAL_Delay(100);
 			BMP_Calibrate();
@@ -88,50 +131,78 @@ void BMP_Init(void){
 	}		
 	LogState(BMP280.Communicator);
 }
-//------------------------------------------------
-static void BMP_GetTemperature(){
+
+/**
+  * @brief Reading temperature data
+  * @note Reading data from BMP280 and writing it to Device Struct
+  * @param None
+  * @retval None
+  */
+static void BMP_GetTemperature(void)
+{
 	uint32_t temper_raw;
 	int32_t val1, val2;
-	while(BMP_Status() & BMP280_IS_UPDATING);
+	while(BMP_IsUpdating()){};
     I2C_ReadDataU24BE(&BMP280.Communicator, BMP280_TEMP_REGISTER, &temper_raw);
 	temper_raw >>= 4;
-	val1 = ((((temper_raw>>3) - ((int32_t)BMP280.Coefficients.dig_T1 << 1))) * ((int32_t)BMP280.Coefficients.dig_T2)) >> 11;
-	val2 = (((((temper_raw>>4) - ((int32_t)BMP280.Coefficients.dig_T1)) * ((temper_raw>>4) - ((int32_t)BMP280.Coefficients.dig_T1))) >> 12) * ((int32_t)BMP280.Coefficients.dig_T3)) >> 14;
+	val1 = (int32_t)((((temper_raw>>3) - ((int32_t)BMP280.Coefficients.dig_T1 << 1))) * ((int32_t)BMP280.Coefficients.dig_T2)) >> 11;
+	val2 = (int32_t)(((((temper_raw>>4) - ((int32_t)BMP280.Coefficients.dig_T1)) * ((temper_raw>>4) - ((int32_t)BMP280.Coefficients.dig_T1))) >> 12) * ((int32_t)BMP280.Coefficients.dig_T3)) >> 14;
 	temper_int = val1 + val2;
-	BMP280.Data.temperature = (double)(((temper_int * 5 + 128) >> 8) / 100.0f);
+	BMP280.Data.temperature = (float)((temper_int * 5 + 128) >> 8) / 100.0f;
 }
-//------------------------------------------------
-static double BMP_GetPressureAndTemperature(){
-  float press_float = 0.0f;
-	uint32_t press_raw, pres_int;
-	int64_t val1, val2, p;
-	BMP_GetTemperature();
-	while(BMP_Status() & BMP280_IS_UPDATING);
+
+/**
+  * @brief Reading preassure and temperature data
+  * @note
+  * Reading data from BMP280 and writing it to Device Struct \n
+  * This function calls BMP_GetTemperature() function because when reading pressure you must read temperature first, so we reading all this stuff together
+  * @param None
+  * @retval None
+  */
+static void BMP_GetPressureAndTemperature(void)
+{
+    float press_float = 0.0f;
+    uint32_t press_raw, pres_int;
+    int64_t val1, val2, p;
+    BMP_GetTemperature();
+    while(BMP_IsUpdating()){};
     I2C_ReadDataU24BE(&BMP280.Communicator, BMP280_PRESSURE_REGISTER, &press_raw);
-	press_raw >>= 4;
-	val1 = ((int64_t) temper_int) - 128000;
-	val2 = val1 * val1 * (int64_t)BMP280.Coefficients.dig_P6;
-	val2 = val2 + ((val1 * (int64_t)BMP280.Coefficients.dig_P5) << 17);
-	val2 = val2 + ((int64_t)BMP280.Coefficients.dig_P4 << 35);
-	val1 = ((val1 * val1 * (int64_t)BMP280.Coefficients.dig_P3) >> 8) + ((val1 * (int64_t)BMP280.Coefficients.dig_P2) << 12);
-	val1 = (((((int64_t)1) << 47) + val1)) * ((int64_t)BMP280.Coefficients.dig_P1) >> 33;
-	p = 1048576 - press_raw;
-	p = (((p << 31) - val2) * 3125) / val1;
-	val1 = (((int64_t)BMP280.Coefficients.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
-	val2 = (((int64_t)BMP280.Coefficients.dig_P8) * p) >> 19;
-	p = ((p + val1 + val2) >> 8) + ((int64_t)BMP280.Coefficients.dig_P7 << 4);
-	pres_int = ((p >> 8) * 1000) + (((p & 0xff) * 390625) / 100000);
-	press_float = (float)pres_int / 1000.0f;
-  return (double)press_float;
+    press_raw >>= 4;
+    val1 = ((int64_t) temper_int) - 128000;
+    val2 = val1 * val1 * (int64_t)BMP280.Coefficients.dig_P6;
+    val2 = val2 + ((val1 * (int64_t)BMP280.Coefficients.dig_P5) << 17);
+    val2 = val2 + ((int64_t)BMP280.Coefficients.dig_P4 << 35);
+    val1 = ((val1 * val1 * (int64_t)BMP280.Coefficients.dig_P3) >> 8) + ((val1 * (int64_t)BMP280.Coefficients.dig_P2) << 12);
+    val1 = (((((int64_t)1) << 47) + val1)) * ((int64_t)BMP280.Coefficients.dig_P1) >> 33;
+    p = 1048576 - press_raw;
+    p = (((p << 31) - val2) * 3125) / val1;
+    val1 = (((int64_t)BMP280.Coefficients.dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+    val2 = (((int64_t)BMP280.Coefficients.dig_P8) * p) >> 19;
+    p = ((p + val1 + val2) >> 8) + ((int64_t)BMP280.Coefficients.dig_P7 << 4);
+    pres_int = ((p >> 8) * 1000) + (((p & 0xff) * 390625) / 100000);
+    press_float = (float)pres_int / 1000.0f;
+    BMP280.Data.pressure = (double)press_float;
+    BMP280.Data.mmHg = BMP280.Data.pressure * 0.00750061683;
 }
-//------------------------------------------------
-void BMP_ReadData(){
-	if (BMP280.Communicator.ConnectionStatus == HAL_OK){
-		BMP280.Data.pressure = BMP_GetPressureAndTemperature();
-		BMP280.Data.mmHg = BMP280.Data.pressure * 0.00750061683;
+
+/**
+  * @brief Reading data
+  * @note
+  * Reading data from BMP280 and writing it to Device Struct \n
+  * Public version to call from main \n
+  * Calls GenerateDataRepresentation() for BMP280
+  * @param None
+  * @retval None
+  */
+void BMP_ReadData(void)
+{
+	if (BMP280.Communicator.ConnectionStatus == HAL_OK)
+    {
+		BMP_GetPressureAndTemperature();
 		BMP280.Data.altitude = (BMP280.Data.base_mmHg - BMP280.Data.mmHg) * 10.5;			
 	}
-	else{
+	else
+    {
 		BMP280.Data.pressure = 0;
 		BMP280.Data.mmHg = 0;
 		BMP280.Data.altitude = 0;
