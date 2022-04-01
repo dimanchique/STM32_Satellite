a03e89c047911639336b2122961836a079368dd1
dimanchique <ghettorunner@yandex.ru>
2022-04-01 08:32:32 +0300

    Improved coding style

--- User/Inc/I2C_Process.h
+++ User/Inc/I2C_Process.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "stm32h7xx_hal.h"
-extern I2C_HandleTypeDef hi2c1;
 
 #define be24toword(a) ((((a)>>16)&0x000000ff)|((a)&0x0000ff00)|(((a)<<16)&0x00ff0000)) //flip MSB and LSB
 #define CommunicationAddress(a) a<<1
 
 #define MAX_I2C_DEVICES 10
 
-/* I2C Bus Struct */
+extern I2C_HandleTypeDef hi2c1;
+
+/** I2C Bus Struct **/
 typedef struct I2C_BusStruct
 {
 	I2C_HandleTypeDef I2C_Instance;
@@ -17,7 +18,7 @@ typedef struct I2C_BusStruct
 	uint8_t Devices;
 } I2C_BusStruct;
 
-/* I2C Device Struct */
+/** I2C Device Struct **/
 typedef struct I2C_DeviceStruct
 {
 	char* Name;	
@@ -30,7 +31,7 @@ typedef struct I2C_DeviceStruct
 	HAL_StatusTypeDef ConnectionStatus;
 } I2C_DeviceStruct;
 
-/* Connection Status Enum */
+/** Connection Status Enum **/
 enum ConnectionStatus
 {
 	NotInitialized,
@@ -45,24 +46,24 @@ enum ConnectionStatus
 	ID_Check_Error
 };
 
-/* Operation Type Enum */
+/** Operation Type Enum **/
 enum Operation
 {
 	Reading,
 	Writing
 };
 
-/* Base Functions Prototypes */
+/** Base Functions Prototypes **/
 void I2C_Init(void);
 void I2C_Scan(void);
 void Verify_Device(I2C_DeviceStruct *Communicator);
 void CheckDeviceState(I2C_DeviceStruct *Communicator);
 
-/* Communication Functions Prototypes */
+/** Communication Functions Prototypes **/
 void I2C_WriteData8(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint8_t Value);
 void I2C_ReadData8(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint8_t *Value);
 void I2C_ReadData3x8(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint8_t *Value);
+void I2C_ReadData6x8(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint8_t* Value);
 void I2C_ReadDataU16(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint16_t *Value);
 void I2C_ReadDataS16(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, int16_t *Value);
 void I2C_ReadDataU24BE(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint32_t *Value);
-void I2C_ReadData6x8(I2C_DeviceStruct *Communicator, uint8_t RegisterAddress, uint8_t* Value);
